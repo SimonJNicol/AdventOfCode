acac9f1eb93b64139a897bdb354924c5f9a9275a
SimonJNicol <simon.j.nicolsemail@gmail.com>
2019-11-27 15:30:09 -0800

    Update TriangleAdventProblem.java

--- TriangleAdventProblem.java
+++ TriangleAdventProblem.java
@@ -1,10 +1,11 @@
-// I have been having trouble getting JDK to run properly on my machine, so I'm not sure if this will compile, but I think it does.
 // I chose not to use subroutines because we never have to use a loop or action more than once, which makes subroutines pointless in 
 // this instance.
-public class Day3 {
+import java.util.Scanner;
+import java.util.*;
+public class TriangleAdventProblem {
 	public static void main (String [] args) {
 		// it would probably be better to use a different scanner that doesn't rely on my machine's file pathing. 
-		Scanner s = new Scanner(new File(C:\cygwin64\home\code\input.txt));
+		Scanner s = new Scanner("input.txt");
  		int i = 0; 
 		int [] tris = new int [16384];
 		while(s.hasNextInt()) {
@@ -16,7 +17,7 @@ public class Day3 {
 		int b = 1;
 		int c = 2;
 		int w = 0; // this is a bit of a copout to just run the loop 16384 times, but it works for this case.
-		while (w < 16384) {
+		while (w < 16383) {
 		if ( tris[a] < tris[b] + tris[c] && tris[b] < tris[a] + tris[c] && tris[c] < tris[a] + tris[b]) {
 		count++; // this loop counts the number of triangles
 		}
