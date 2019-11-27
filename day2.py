74d153e73c69ef9c70124d456f0a33a0979e14a7
SimonJNicol <simon.j.nicolsemail@gmail.com>
2019-11-27 11:37:37 -0800

    Add files via upload

--- day2.py
+++ day2.py
@@ -2,12 +2,12 @@ def counter(ch, s, accum):
  while c < len(s)
   if not s:
    return accum
-   c++ 
+   c = c + 1 
   if s.find(ch) == -1:
    return accum
-   c++ 
+   c = c + 1 
   else:
    ch = s.find(ch+1) 
-   accum++
+   accum = accum + 1
    c++
  return accum
