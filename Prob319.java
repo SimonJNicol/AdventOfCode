95d459ec9fdb325796607a3dc73d4b5a84b7eb44
SimonJNicol <simon.j.nicolsemail@gmail.com>
2019-12-03 20:24:55 -0800

    Attempted to store all points in a list, ran out of memory
    Dedicated 16gb of ram to JVM and still run out of memory before populating the list...

--- Prob319.java
+++ Prob319.java
@@ -1,15 +1,18 @@
 import java.io.*;
 import java.lang.String;
 import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Prob319 {
 	
  	public static void main (String[] args) throws IOException {
 		
 		String[] listing = reader();
-        String[] dclist = listing[0].split(",");
-//		int answer = checker(listing);
-		System.out.println(dclist[0]); 
+        String[] dclist = decrypter(listing, 0);
+        String[] dclist2 = decrypter(listing, 1);
+		int answer = distance(dclist, dclist2);
+		System.out.println(answer); 
 
 	}
 		
@@ -24,9 +27,9 @@ public class Prob319 {
 	   // https://stackoverflow.com/questions/5868369/how-to-read-a-large-text-file-line-by-line-using-java
 		
 		try(BufferedReader br = new BufferedReader(new FileReader("input.txt"))) {
-     			for(String line; (line = br.readLine()) != null; ) {
-     				temp[i] = line;
-       				i++;
+     		for(String line; (line = br.readLine()) != null; ) {
+     			temp[i] = line;
+       			i++;
 			}
 		}
 
@@ -38,12 +41,170 @@ public class Prob319 {
 		return listing;
 	}
     
-//    public static int distance () {
+    public static int distance (String[] dclist, String[] dclist2) {
 
-//    }
+        int temp = 0; //use temp to iterate through the x and y arrays
+        int xc = 0;
+        int yc = 0;
+        int xc2 = 0;
+        int yc2 = 0;
+//        int cordsize = 0;
+        int count = 0;
+        int count2 = 0;
+        int[] location = {0,0};
+//        cordsize = asize(dclist, dclist2);
 
-//    public static String[] decrypter (String[] listing) {
+//        int[] x = new int[cordsize];
+//        int[] y = new int[cordsize];
+//        int[] x2 = new int[cordsize];
+//        int[] y2 = new int[cordsize];
+//        String x = "";
+//        String y = "";
+//        String x2 = "";
+//        String y2 = "";
+        // thought about using StringBuilder to store the arrays with dynamic sizing, but found a better option
+        //https://www.geeksforgeeks.org/list-interface-java-examples/
 
-//    }
-    
+        List<Integer> x = new ArrayList<Integer>();
+        List<Integer> y = new ArrayList<Integer>();
+        List<Integer> x2 = new ArrayList<Integer>();
+        List<Integer> y2 = new ArrayList<Integer>();
+
+        for (int i = 0; i < dclist.length; i++) {
+            switch (dclist[i].charAt(0)) {
+                case 'R':
+                    while (xc < xc + Integer.parseInt(dclist[i].substring(1, dclist[i].length()))) {
+//                        x[xc] = xc;
+//                        xc++;
+                        x.add(count, xc);
+                        y.add(count, yc);
+                        xc++;
+                        count++;
+                    }
+                    break;
+                case 'L': 
+                    while (xc < xc + Integer.parseInt(dclist[i].substring(1, dclist[i].length()))) {
+//                        x[xc] = xc;
+//                        xc--;
+                        x.add(count, xc);
+                        y.add(count, yc);
+                        xc--;
+                        count++;
+                    }
+                    break;
+                case 'U': 
+                    while (yc < yc + Integer.parseInt(dclist[i].substring(1, dclist[i].length()))) {
+//                        y[yc] = yc;
+//                        yc++;
+                        x.add(count, xc);
+                        y.add(count, yc);
+                        yc++;
+                        count++;
+                    }
+                case 'D': 
+                    while (yc < yc + Integer.parseInt(dclist[i].substring(1, dclist[i].length()))) {
+//                        y[yc] = yc;
+//                        yc--;
+                        x.add(count, xc);
+                        y.add(count, yc);
+                        yc--;
+                        count++;
+                    }
+                    break;
+            }
+        }
+
+        for (int i = 0; i < dclist2.length; i++) {
+            switch (dclist2[i].charAt(0)) {
+                case 'R': 
+                    while (xc2 < xc2 + Integer.parseInt(dclist2[i].substring(1, dclist2[i].length()))) {
+//                        x[xc2] = xc2;
+//                        xc2++;
+                        x2.add(count2, xc2);
+                        y2.add(count2, yc2);
+                        xc2++;
+                        count2++;
+                    }
+                    break;
+                case 'L': 
+                    while (xc2 < xc2 + Integer.parseInt(dclist2[i].substring(1, dclist2[i].length()))) {
+//                        x[xc2] = xc2;
+//                        xc2--;
+                        x2.add(count2, xc2);
+                        y2.add(count2, yc2);
+                        xc2--;
+                        count2++;
+                    }
+                    break;
+                case 'U': 
+                    while (yc2 < yc2 + Integer.parseInt(dclist2[i].substring(1, dclist2[i].length()))) {
+//                        y[yc2] = yc2;
+//                        yc2++;
+                        x2.add(count2, xc2);
+                        y2.add(count2, yc2);
+                        yc2++;
+                        count2++;
+                    }
+                case 'D': 
+                    while (yc2 < yc2 + Integer.parseInt(dclist2[i].substring(1, dclist2[i].length()))) {
+//                        y[yc2] = yc2;
+//                        yc2--;
+                        x2.add(count2, xc2);
+                        y2.add(count2, yc2);
+                        yc2--;
+                        count2++;
+                    }
+            }
+        }
+        // iF yoU'rE rEaDinG thIs PlEASe HelP
+        for (int i = 0; i < x.size(); i++) {
+            for (int k = 0; k < x.size(); k++) {
+                if (x.get(k) == x2.get(i) && y.get(k) == y2.get(i) && location[0] == 0 && location[1] == 0) {
+                    location[0] = x.get(k); 
+                    location[1] = y.get(k);
+                }
+                else if (x.get(k) == x2.get(i) && y.get(k) == y2.get(i) && Math.abs(x.get(k) + y.get(k)) < Math.abs(location[0] + location[1])) {
+                    location[0] = x.get(k);
+                    location[1] = y.get(k);
+                    System.out.println("[" + location[0] + "," + location[1] + "]");
+                }
+            }
+        }
+        return location[0] + location[1];
+    }
+
+    public static String[] decrypter (String[] listing, int wire) {
+        String[] dclist = listing[wire].split(","); //https://www.geeksforgeeks.org/split-string-java-examples/
+        return dclist;
+    }
+
+/*    public static int asize (String[] dclist, String[] dclist2) {
+        int count = 0;
+        for (int i = 0; i < dclist.length; i++) {
+            switch(dclist[i].charAt(0)) {
+                case 'R':
+                    count += Integer.parseInt(dclist2[i].substring(1, dclist2[i].length()));
+                case 'L':
+                    count += Integer.parseInt(dclist2[i].substring(1, dclist2[i].length()));
+                case 'U':
+                    count += Integer.parseInt(dclist2[i].substring(1, dclist2[i].length()));
+                case 'D':
+                    count += Integer.parseInt(dclist2[i].substring(1, dclist2[i].length()));
+            }
+        }
+        for (int i = 0; i < dclist.length; i++) {
+            switch(dclist2[i].charAt(0)) {
+                case 'R':
+                    count += Integer.parseInt(dclist2[i].substring(1, dclist2[i].length()));
+                case 'L':
+                    count += Integer.parseInt(dclist2[i].substring(1, dclist2[i].length()));
+                case 'U':
+                    count += Integer.parseInt(dclist2[i].substring(1, dclist2[i].length()));
+                case 'D':
+                    count += Integer.parseInt(dclist2[i].substring(1, dclist2[i].length()));
+            }
+        }
+    System.out.println(count);
+    return count;
+    } */
 }
