5f4634c3f00fb9848ecfc6d03d24a492bea31724
SimonJNicol <simon.j.nicolsemail@gmail.com>
2019-12-05 23:53:39 -0800

    arrays populating, seems to be on the right track

--- Prob619.java
+++ Prob619.java
@@ -4,16 +4,56 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
 
-public class Prob319 {
+public class Prob619 {
 	
  	public static void main (String[] args) throws IOException {
 		
 		String[] listing = reader();
-		int answer = distance(decrypter(listing, 0), decrypter(listing, 1));
-		System.out.println("The closest cross has a manhattan distance of: " + answer); 
-
-	}
+        int size = listing.length;
+        int count = 0;
+        String line = "";
+		for (int i = 0; i < listing.length; i++) {
+			line = line.concat(listing[i]);
+            line = line.concat(")");
+        }
 		
+        String[] splitList = line.split("\\)");
+
+//      https://stackoverflow.com/questions/16956720/how-to-create-an-2d-arraylist-in-java
+       
+        List<List<String>> planets = new ArrayList<List<String>>(size); 
+
+        for(int i = 0; i < size*2; i += 2) {
+            planets.add(new ArrayList<String>());
+            planets.get(i/2).add(splitList[i]);
+            planets.get(i/2).add(splitList[i+1]);
+
+        }
+
+        for(int i = 0; i < planets.size(); i++) {
+            count += orbits(planets, i, 0);
+        }
+
+        System.out.println(count);
+	}
+	
+    public static int orbits (List<List<String>> planets, int i, int subcount) {
+        subcount += 1;
+        
+        for(int c = 0; c < planets.size(); c++) {
+            if (planets.get(i).get(0).equals(planets.get(c).get(1))) {
+                subcount += 1;
+                i = c; //might be a problem with using the proper index
+                System.out.print(planets.get(i).get(0) + " is connected to " + planets.get(c).get(1) + " ");
+                break;
+            }
+            else if (c == planets.size()-1) {
+                System.out.println("---");
+                return subcount;
+            }
+        }
+        return orbits(planets, i, subcount);
+    }
 		// minus a couple changes, the reader class is copied from Ian's github.
 
 	public static String[] reader () throws IOException {
