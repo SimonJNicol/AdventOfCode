e9bcf51d29d62e671e2208c1b96bcba79648d3d9
SimonJNicol <simon.j.nicolsemail@gmail.com>
2019-12-12 15:28:20 -0800

    Add files via upload

--- Prob1219.java
+++ Prob1219.java
@@ -11,14 +11,12 @@ public class Prob1219 {
         Planet Callisto = (Planet) Io.clone();
         Planet Ganymede = (Planet) Io.clone();
         Planet Europa = (Planet) Io.clone();
-        int[] count = new int[4];
         Io.initPlanet(13,-13,-2);
         Callisto.initPlanet(16,2,-15);
         Ganymede.initPlanet(7,-18,-12);
         Europa.initPlanet(-3,-8,-8);
             
         for (int i = 0; i < 1000; i++) {
-
             Io.updateVelocity(Callisto, Ganymede, Europa);
             Callisto.updateVelocity(Ganymede, Europa, Io);
             Ganymede.updateVelocity(Callisto, Io, Europa);
@@ -28,24 +26,19 @@ public class Prob1219 {
             Callisto.updatePosition(Callisto);
             Ganymede.updatePosition(Ganymede);
             Europa.updatePosition(Europa);
+        }
+    System.out.println(Io.getEnergy()+Callisto.getEnergy()+Ganymede.getEnergy()+Europa.getEnergy()); //sum of total energy (the answer)
+    }
 
-            System.out.println("Step: " + i);
+
+/*            System.out.println("Step: " + i);
             Io.print();
             Callisto.print();
             Ganymede.print();
             Europa.print();
-
-        }
-    count[0] = Io.getEnergy();
-    count[1] = Callisto.getEnergy();
-    count[2] = Ganymede.getEnergy();
-    count[3] = Europa.getEnergy();
-    System.out.println(count[0]+count[1]+count[2]+count[3]);
-    }
-
-
-
-/*	public static String[] reader() throws IOException {
+*/
+//        }
+	public static String[] reader() throws IOException {
    		String[] temp = new String[4];
    		int i=0;
 
@@ -65,31 +58,29 @@ public class Prob1219 {
 		
 		return listing;
 
-	} */
+	}
 }
 class Planet implements Cloneable{ //https://stackoverflow.com/questions/46724212/change-values-in-object-java
     int px, py, pz, vx, vy, vz;
-    
-    public Planet () {
-        this.px = 0;
-        this.py = 0;
-        this.pz = 0;
+    public Planet () { //all planets start with total velocity 0
         this.vx = 0;
         this.vy = 0;
         this.vz = 0;
     }
+    //because objects must be created as clones, planets must be initialized in a seperate clause.
     public void initPlanet (int px, int py, int pz) {
         this.px = px;
         this.py = py;
-        this.pz = pz;
+        this.pz = pz; 
     }
-    public void updatePosition (Planet a) {
+    public void updatePosition (Planet a) { //applies velocity to position
         this.px += this.vx;
         this.py += this.vy;
         this.pz += this.vz;
     }
-
-    public void updateVelocity (Planet a, Planet b, Planet c) {
+    //updateVelocity() checks the current planets position versus the other planet's positions
+    //and increments velocity accordingly.
+    public void updateVelocity (Planet a, Planet b, Planet c) { 
         int[] xCords = new int[3];
         int[] yCords = new int[3];
         int[] zCords = new int[3];
@@ -117,12 +108,10 @@ class Planet implements Cloneable{ //https://stackoverflow.com/questions/4672421
                 this.vz += 1;
         }
     }
-    
-    public int getEnergy () {
+    public int getEnergy () { //returns the product of potential vs kinetic energy
         return (Math.abs(this.vx) + Math.abs(this.vy) + Math.abs(this.vz)) * (Math.abs(this.px) + Math.abs(this.py) + Math.abs(this.pz));
     }
-
-    public void print () {
+    public void print () { //prints the planet's current position and velocity
         System.out.print("Pos: <x = " + this.px + "> <y = " + this.py + "> <z = " + this.pz + ">   ");
         System.out.println("Vel: <x = " + this.vx + "> <y = " + this.vy + "> <z = " + this.vz + ">");
     }
