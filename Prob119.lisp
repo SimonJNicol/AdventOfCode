3285302a89eaff15cdaddec4c348f7f41158535e
SimonJNicol <simon.j.nicolsemail@gmail.com>
2019-12-13 13:37:18 -0800

    Add files via upload

--- Prob119.lisp
+++ Prob119.lisp
@@ -1,3 +1,4 @@
+
 ; https://github.com/jamesread/advent-of-code-2019-lisp/blob/master/day1.1/main.lisp
 ; syntax was grabbed from this repository, and a series of youtube videos from O'Reilly,
 ; part 2 can be found here https://www.youtube.com/watch?v=jvnwXHsL8eo
@@ -7,8 +8,8 @@
 ; In this case, floor truncates our return from mass/3
 (defvar fuelRequired 0)
 
-(with-open-file (in "input") ;from the github link at the top
+(with-open-file (in "input.txt") ;from the github link at the top
     (loop for line = (read-line in nil) while line do (
-        incf fuelRequired (calcFuel (parse-integer line)))))
+        incf fuelRequired (calcFuel (parse-integer line))))) ;incf is the same as +=
 
-(format t "Total fuel needed: ~a~%" fuelRequired)
+(format t "Part 1, fuel required: ~a~%" fuelRequired)
