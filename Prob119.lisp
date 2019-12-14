5822da6bde2e11c3a7ff802d06095d63c4c5ee32
SimonJNicol <simon.j.nicolsemail@gmail.com>
2019-12-13 19:42:19 -0800

    added comments

--- Prob119.lisp
+++ Prob119.lisp
@@ -3,13 +3,13 @@
 ; syntax was grabbed from this repository, and a series of youtube videos from O'Reilly,
 ; part 2 can be found here https://www.youtube.com/watch?v=jvnwXHsL8eo
 (defun calcFuel (mass)
-    (- (floor (/ mass 3)) 2))
+    (- (floor (/ mass 3)) 2)) ;fuel equation from the problem.
 ; Definition of floor function from google: Simplified Common Lisp reference - floor. FLOOR function returns two values, the first is result of dividing number by divisor and truncating toward negative infinity. Second result remainder that satisfies equation: quotient * divisor + remainder = number.
 ; In this case, floor truncates our return from mass/3
 (defvar fuelRequired 0)
 
 (with-open-file (in "input.txt") ;from the github link at the top
     (loop for line = (read-line in nil) while line do (
-        incf fuelRequired (calcFuel (parse-integer line))))) ;incf is the same as +=
+        incf fuelRequired (calcFuel (parse-integer line)))))
 
 (format t "Part 1, fuel required: ~a~%" fuelRequired)
