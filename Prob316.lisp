794a5d1dfda84c136dde87f6a2de0dfb9cc84aee
SimonJNicol <simon.j.nicolsemail@gmail.com>
2019-12-18 17:39:32 -0800

    Shell in place, not quite working yet, problem with if

--- Prob316.lisp
+++ Prob316.lisp
@@ -1,12 +1,17 @@
-(defun is-triangle (side-a side-b side-c)
-    (format t "Part 1, triangles ~a~%" side-a)
-    (format t "Part 1, triangles ~a~%" side-b)
-    (format t "Part 1, triangles ~a~%" side-c))
+(defvar count 0)
+(defun is-triangle (a b c)
+    (if 
+        (> (* (+ a b c) (- (+ a b c) a) (- (+ a b c) b) (- (+ a b c) c)) 0)
+        (return-from is-triangle 1)
+        (return-from is-triangle 0)))
 
 
 (with-open-file (in "input.txt")
     (loop for line = (read-line in nil) while line do 
-        (is-triangle 
-            (parse-integer (subseq line 2 5) :junk-allowed t) 
-            (parse-integer (subseq line 7 10) :junk-allowed t) 
-            (parse-integer (subseq line 12 15) :junk-allowed t))))
+        (incf count
+            (is-triangle 
+                (parse-integer (subseq line 2 5) :junk-allowed t) 
+                (parse-integer (subseq line 7 10) :junk-allowed t) 
+                (parse-integer (subseq line 12 15) :junk-allowed t)))))
+
+(format t "Number of triangles for part 1: ~a~%" count)
